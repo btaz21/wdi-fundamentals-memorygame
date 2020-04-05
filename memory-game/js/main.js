1bba95befdadfdeb6ab64430b012a5414c96b6ab
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-04-05 14:37:44 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,14 +1,27 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");} 
+	else {console.log("Sorry, try again.");}
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();}
+};
+flipCard(3);
+flipCard(2);
+
+
+/*
 const cardOne = cards[3];
 cardsInPlay.push(cardOne);
 console.log("User flipped queen");
+
 const cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped king");
+*/
 
-if (cardsInPlay.length === 2)
-{
-	if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");} 
-	else {alert("Sorry, try again.");}
-}
+
